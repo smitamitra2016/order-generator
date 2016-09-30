2113be9772058e5b9cf65a2891fe57c2f32fd0b2
Smita Mitra <smita.mitra@nomadrail.com>
2016-09-30 13:38:34 +0100

    First commit

---
+++ src/main/java/com/example/OrderGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderGeneratorApplication.class, args);
+	}
+}
